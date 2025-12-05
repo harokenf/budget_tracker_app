328d08b5714f7ed0e0cfb5f1811697b38d54fb13
Guiadem Clarisse <clarisseguiadem071@gmail.com>
2025-12-05 23:32:07 +0100

    feat: add route

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('home.html')
+    return render_template('dashboard.html')
+
+@app.route('/expense', methods=['GET', 'POST'])
+def expense():
+    return render_template('expense.html')
+
+@app.route('/reports')
+def reports():
+    return render_template('reports.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
